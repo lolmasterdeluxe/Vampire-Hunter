2ad88521b676e1b10d719c6d089db21a659731ae
JLT3k <junliangho@gmail.com>
2022-02-14 16:41:44 +0800

    Update SimplePlayerControls.cs
    added 3rd person cam

--- Assets/Scripts/SimplePlayerControls.cs
+++ Assets/Scripts/SimplePlayerControls.cs
@@ -6,15 +6,28 @@ public class SimplePlayerControls : MonoBehaviour
 {
     // Update is called once per frame
     Rigidbody rb;
-    Camera camera;
+
+    float camXrot = 0;
+    float camYrot = 0;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        camera = Camera.main;
     }
     private void Update()
     {
-        camera.transform.position = rb.position + Vector3.back * 5;
+        //if (Input.GetKey(KeyCode.Mouse1))
+        {
+            float mouseXmovement = Input.GetAxis("Mouse X");
+            float mouseYmovement = Input.GetAxis("Mouse Y");
+            camXrot += mouseXmovement;
+            camYrot -= mouseYmovement;
+        }
+
+        camYrot = Mathf.Clamp(camYrot, -90f, 90f);
+
+        Camera.main.transform.rotation = Quaternion.Euler(camYrot, camXrot, 0);
+
+        Camera.main.transform.position = transform.position - Camera.main.transform.forward * 5f;
     }
     void FixedUpdate()
     {
