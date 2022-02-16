5fa1e533117ca2e2b38d397bcb107630e621edb4
lolmaster deluxe <kiddrifdi@gmail.com>
2022-02-17 04:28:35 +0800

    Added weapons and weapon animations
    Press m1 multiple times to combo

--- Assets/Scripts/BasicMeleeWeapon.cs
+++ Assets/Scripts/BasicMeleeWeapon.cs
@@ -4,9 +4,59 @@ using UnityEngine;
 
 public class BasicMeleeWeapon : MeleeWeapon
 {
+    public Animator playerAnimation;
+    public GameObject target;
+    bool comboPossible;
+    int comboStep;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack(target);
+        }
+    }
     public override void Attack(GameObject target)
     {
-        Debug.Log("Melee Weapon Attack.");
-        DealDamage(target, itemInfo.damage);
+        if (comboStep == 0)
+        {
+            playerAnimation.Play("Attack1");
+            comboStep = 1;
+            Debug.Log("Melee Weapon Attack.");
+            DealDamage(target, itemInfo.damage);
+            return;
+        }
+        if (comboStep != 0)
+        {
+            if (comboPossible)
+            {
+                comboPossible = false;
+                comboStep += 1;
+            }
+        }
     }
+
+    public void ComboPossible()
+    {
+        comboPossible = true;
+    }
+    public void Combo(GameObject target)
+    {
+        if (comboStep == 2)
+        {
+            playerAnimation.Play("Attack2");
+            DealDamage(target, itemInfo.damage);
+        }
+        if (comboStep == 3)
+        {
+            playerAnimation.Play("Attack3");
+            DealDamage(target, itemInfo.damage);
+        }
+    }
+    public void ComboReset()
+    {
+        comboPossible = false;
+        comboStep = 0;
+    }
+        
 }
