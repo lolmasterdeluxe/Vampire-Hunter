c799af46dc63a4adfd5f8bab90d86d5e7fef4187
number10203 <69341783+number10203@users.noreply.github.com>
2022-02-17 11:39:54 +0800

    Carriage update

--- Assets/Scripts/CarriageInteraction.cs
+++ Assets/Scripts/CarriageInteraction.cs
@@ -9,6 +9,10 @@ public class CarriageInteraction : MonoBehaviour
     public GameObject text;
     public GameObject panel;
     public GameObject camera;
+    public GameObject shop;
+    public GameObject travel;
+    //public GameObject travelDestination;
+    public GameObject player;
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -32,7 +36,11 @@ public class CarriageInteraction : MonoBehaviour
         {
             ShowPanel();
         }
-        else if (triggerActive && panel.activeSelf == false)
+        else if(triggerActive && Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitCarriage();
+        }
+        else if (triggerActive && panel.activeSelf == false && shop.activeSelf == false && travel.activeSelf == false)
         {
             ShowHint();
         }
@@ -55,10 +63,27 @@ public class CarriageInteraction : MonoBehaviour
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = false;
+        //GameObject.Find("Player").GetComponent<PlayerMovement>().direction
     }
-    public void NoMouse()
+    public void QuitCarriage()
     {
+        panel.SetActive(false);
+        shop.SetActive(false);
+        travel.SetActive(false);
+        camera.SetActive(true);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
+    }
+
+    public void Teleport(int index)
+    {
+        string target = "Carriage_" + index;
+        //Debug.Log("target" + target);
+        GameObject travelDestination = GameObject.Find(target);
+        Vector3 targetPosition = travelDestination.transform.position;
+        player.transform.position = targetPosition + new Vector3(-40,0,3);
+        QuitCarriage();
+        //GameObject.Find("Carriage_place1").GetComponent<Position>
     }
 }
