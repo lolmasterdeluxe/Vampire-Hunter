e4803d42f57ceef9411dbda3eb93e58550937712
JLT3k <junliangho@gmail.com>
2022-02-21 10:58:50 +0800

    merge

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerStats
+{
+    public static float Vitality;
+    public static float Endurance;
+    public static float Strength;
+    public static float Dexterity;
+    public static float Health;
+    public static float Stamina;
+    public static float Defense;
+    public static float Weapon1Dmg;
+    public static float Weapon2Dmg;
+}
