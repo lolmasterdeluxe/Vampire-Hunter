7a95cc21b1e59dae37933b10010f858f20aee7c1
JLT3k <junliangho@gmail.com>
2022-02-27 20:31:08 +0800

    added pickup particle effect

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -4,24 +4,21 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    public Item holding;
-    //[SerializeField]
-    public GameObject placeholder;
-    
+    public Item holding;    
 
-    private void Awake()
-    {
-        if (holding != null && holding.model3D != null)
-        {
-            Debug.Log("has model");
-            Instantiate(holding.model3D, gameObject.transform);
-        }
-        else
-        {
-            Debug.Log("no model");
-            placeholder.SetActive(true);
-        }
-    }
+    //private void Awake()
+    //{
+    //    if (holding != null && holding.model3D != null)
+    //    {
+    //        Debug.Log("has model");
+    //        Instantiate(holding.model3D, gameObject.transform);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("no model");
+    //        placeholder.SetActive(true);
+    //    }
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
